6525047f626d031117e4b89c4f4ce3c5467e5214
Kaktus1549 <hummelotup@seznam.cz>
2024-02-11 20:00:39 +0100

    Better error messages

--- README.md
+++ README.md
@@ -4,10 +4,6 @@ Website for film voting
 
 !!! DULEZITE !!!
 
-Pridat overeni ze dany uzivatel opravdu existuje v db, JINAK 401!!
-
-Better error messages
-
 Vote time (zacatek, konec etc)
 
 Remove testing and atesting
--- backend/route.py
+++ backend/route.py
@@ -64,7 +64,7 @@ def login():
     session.close()
     if token == False:
         return jsonify({"error": "Invalid username or password"}), 401
-    return jsonify({"token": token})
+    return jsonify({"token": token}), 200
 
 
 # REMOVES THIS ROUTES IN PRODUCTION
@@ -82,27 +82,27 @@ def vote():
     token = request.cookies.get("token")
     if token == None:
         pass
-        return jsonify({"error": "401"}), 401
+        return jsonify({"error": "Token not found"}), 401
     session = sessionmaker(bind=engine)()
     user, isAdmin = decode_jwt(jwt_settings["secret"], token, session, jwt_settings["algorithm"], jwt_settings["issuer"])
     if user == None:
-        return jsonify({"error": "401"}), 401
+        return jsonify({"error": "Failed to authenticate"}), 401
     
     if request.method == 'GET':
         if voting['voteInProgress'] == False:
-            return jsonify({"error": "425"}), 425
+            return jsonify({"error": "Voting has not started"}), 425
         # Returns json with all films
         session = sessionmaker(bind=engine)()
         films = unsorted_films(session)
         session.close()
         if films == False:
-            return jsonify({"error": "500"}), 500
-        return jsonify(films)
+            return jsonify({"error": "Could not retrieve films"}), 500
+        return jsonify(films), 200
     elif request.method == 'POST':
         if isAdmin:
-            return jsonify({"error": "403"}), 403
+            return jsonify({"error": "Admins can't vote"}), 403
         if voting['voteInProgress'] == False:
-            return jsonify({"error": "425"}), 425
+            return jsonify({"error": "Voting has not started"}), 425
         # User sends a vote -> {"vote": 1}
         data = request.get_json()
         vote = data["vote"]
@@ -110,20 +110,20 @@ def vote():
         response = vote_film(vote, user, session)
         session.close()
         if response == False:
-            return jsonify({"error": "500"}), 500
-        return jsonify({"message": "200"}), 200
+            return jsonify({"error": "Failed to vote"}), 500
+        return jsonify({"message": "OK"}), 200
 
 @app.route('/scoreboard', methods=['POST'])
 def scoreboard():
     token = request.cookies.get("token")
     if token == None:
-        return jsonify({"error": "401"}), 401
+        return jsonify({"error": "Token not found"}), 401
     session = sessionmaker(bind=engine)()
     user, isAdmin = decode_jwt(jwt_settings["secret"], token, session, jwt_settings["algorithm"], jwt_settings["issuer"])
     if user == None:
-        return jsonify({"error": "401"}), 401
+        return jsonify({"error": "Failed to authenticate"}), 401
     if isAdmin == False:
-        return jsonify({"error": "403"}), 403
+        return jsonify({"error": "Access denied"}), 403
     
     # if voteEnd is null, start voting and returns unsorted films
     # if voteEnd is not null, and voteEnd is in future, returns unsorted films + remaining time
@@ -138,62 +138,62 @@ def scoreboard():
         films = unsorted_films(session)
         session.close()
         if films == False:
-            return jsonify({"error": "500"}), 500
-        return jsonify({"voteEnd": end, "voteDuration": voting["voteDuration"], "films": films})
+            return jsonify({"error": "Failed to retrieve films"}), 500
+        return jsonify({"voteEnd": end, "voteDuration": voting["voteDuration"], "films": films}), 200
     elif voting['voteEnd'] > datetime.datetime.now():
         films = unsorted_films(session)
         session.close()
         remaining = voting['voteEnd'] - datetime.datetime.now()
         remaining = remaining.total_seconds()
         if films == False:
-            return jsonify({"error": "500"}), 500
-        return jsonify({"voteEnd": voting['voteEnd'], "voteDuration": remaining, "films": films})
+            return jsonify({"error": "Failed to retrieve films"}), 500
+        return jsonify({"voteEnd": voting['voteEnd'], "voteDuration": remaining, "films": films}), 200
     else:
         films = sorted_films(session)
         session.close()
         if films == False:
-            return jsonify({"error": "500"}), 500
-        return jsonify({"voteEnd": False, "films": films})
+            return jsonify({"error": "Failed to retrieve films"}), 500
+        return jsonify({"voteEnd": False, "films": films}), 200
 
 @app.route('/pdf', methods=['GET'])
 def pdf():
     requestedPdf = request.args.get('user', default=None, type=str)
     if requestedPdf == None:
-        return jsonify({"error": "400"}), 400
+        return jsonify({"error": "Missing pdf parameter"}), 400
     token = request.cookies.get("token")
     if token == None:
-        return jsonify({"error": "401"}), 401
+        return jsonify({"error": "Token not found"}), 401
     session = sessionmaker(bind=engine)()
     user, isAdmin = decode_jwt(jwt_settings["secret"], token, session, jwt_settings["algorithm"], jwt_settings["issuer"])
     if user == None:
-        return jsonify({"error": "401"}), 401
+        return jsonify({"error": "Failed to authenticate"}), 401
     if isAdmin == False:
-        return jsonify({"error": "403"}), 403
+        return jsonify({"error": "Access denied"}), 403
     # Returns pdf
     pdfPath = pdfs_settings["path"] + requestedPdf + ".pdf"
     try:
         with open(pdfPath, 'rb') as f:
-            return send_file(pdfPath)
+            return send_file(pdfPath), 200
     except FileNotFoundError or IsADirectoryError:
-        return jsonify({"error": "404"}), 404
+        return jsonify({"error": "PDF not found"}), 404
     except Exception as e:
         print(f"An error ocurred while trying to open the file {pdfPath}: {e}")
-        return jsonify({"error": "500"}), 500
+        return jsonify({"error": "An error ocurred while trying to open the file"}), 500
 
 @app.route('/managment', methods=['POST'])
 def managment():
     token = request.cookies.get("token")
     if token == None:
-        return jsonify({"error": "401"}), 401
+        return jsonify({"error": "Token not found"}), 401
     session = sessionmaker(bind=engine)()
     user, isAdmin = decode_jwt(jwt_settings["secret"], token, session, jwt_settings["algorithm"], jwt_settings["issuer"])
     if user == None:
-        return jsonify({"error": "401"}), 401
+        return jsonify({"error": "Failed to authenticate"}), 401
     if isAdmin == False:
-        return jsonify({"error": "403"}), 403
+        return jsonify({"error": "Access denied"}), 403
     request_data = request.get_json()
     if request_data == None:
-        return jsonify({"error": "400"}), 400
+        return jsonify({"error": "The request does not contain a JSON body"}), 400
     action = request_data["action"]
     action_data = request_data["data"]
     if action == "reset":
@@ -202,16 +202,16 @@ def managment():
         user = user_reset(session)
         session.close()
         if film == False or user == False:
-            return jsonify({"error": "500"}), 500
-        return jsonify({"message": "200"}), 200
+            return jsonify({"error": "Failed to reset"}), 500
+        return jsonify({"message": "OK"}), 200
     if action == "remove_film":
         film_id = action_data["film_id"]
         session = sessionmaker(bind=engine)()
         film = remove_film(session, film_id)
         session.close()
         if film == False:
-            return jsonify({"error": "500"}), 500
-        return jsonify({"message": "200"}), 200
+            return jsonify({"error": "Failed to remove film"}), 500
+        return jsonify({"message": "OK"}), 200
     if action == "add_film":
         tilte = action_data["title"]
         team = action_data["team"]
@@ -219,8 +219,8 @@ def managment():
         film = add_film(session, tilte, team)
         session.close()
         if film == False:
-            return jsonify({"error": "500"}), 500
-        return jsonify({"message": "200"}), 200
+            return jsonify({"error": "Failed to add film"}), 500
+        return jsonify({"message": "OK"}), 200
     if action == "remove_user":
         user_id = action_data["user_id"]
         isAdmin = action_data["isAdmin"]
@@ -228,8 +228,8 @@ def managment():
         user = remove_user(session, user_id, isAdmin)
         session.close()
         if user == False:
-            return jsonify({"error": "500"}), 500
-        return jsonify({"message": "200"}), 200
+            return jsonify({"error": "Failed to remove user"}), 500
+        return jsonify({"message": "OK"}), 200
     if action == "add_user":
         isAdmin = action_data["isAdmin"]
         try:
@@ -242,13 +242,13 @@ def managment():
         if isAdmin == False:
             user = add_user(session, isAdmin, username, password, pdfs_settings, jwt_settings)
             pdfUrl = pdfs_settings['pdfUrl'] + f"?user={user}"
-            message = {"message": "200", "pdfUrl": pdfUrl}
+            message = {"message": "OK", "pdfUrl": pdfUrl}
         else:
             if username == None or password == None:
-                return jsonify({"error": "400"}), 400
+                return jsonify({"error": "Missing username or password"}), 400
             user = add_user(session, isAdmin, username, password)
-            message = {"message": "200"}
+            message = {"message": "OK"}
         session.close()
         if user == False:
-            return jsonify({"error": "500"}), 500
+            return jsonify({"error": "Failed to add user"}), 500
         return jsonify(message), 200
