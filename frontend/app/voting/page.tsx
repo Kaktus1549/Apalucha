debe178071c00d03d4d47a578c602f20a9cf3d23
Kaktus1549 <hummelotup@seznam.cz>
2024-02-13 17:10:36 +0100

    token

--- frontend/app/voting/page.tsx
+++ frontend/app/voting/page.tsx
@@ -1,7 +1,14 @@
 import '../style/voting.css'
+import { cookies } from 'next/headers'
 
-export  default async function Voting(){
-    
+export default async function Voting(){
+    const cookieStore = cookies()
+    const token = cookieStore.get('token')
+
+    let data = await fetch('https://apalucha.kaktusgame.eu/api/voting', { headers: { 'Cookie': `token=${token}` } })
+    data = await data.json()
+    console.log(data)
+
     return(
         <div className="main-container">
         <h1>Koho dnes zvolíš?</h1>
