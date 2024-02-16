6c770bb06bdf876d428b6f5ce3dcd937400e19e5
Kaktus1549 <hummelotup@seznam.cz>
2024-02-16 10:35:30 +0100

    errors

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,14 +1,54 @@
 'use client';
 
 import '../style/login.css'
-import { redirect, useSearchParams } from 'next/navigation'
-import Cookie from 'js-cookie';
+import { useSearchParams } from 'next/navigation'
 
+
+async function login(username: string | null, password: string | null, token: string | null){
+    if (username === null && password === null && token === null){
+        return;
+    }
+    let jsonData;
+    if (username === "" && password === "" || password === null && username === null){
+        jsonData = {
+            token: token
+        }
+    }
+    else{
+        jsonData = {
+            username: username,
+            password: password
+        }
+    }
+    const response = await fetch('https://apalucha.kaktusgame.eu/api/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(jsonData)
+    });
+    const data: APIResponse = await response.json() as APIResponse;
+    if (data.message === 'OK'){
+        if (token !== null){
+            window.location.href = '/voting';
+        }
+        else{
+            window.location.href = '/voting';
+        }
+    }
+    else{
+        alert("Něco se pokazilo, zkuste to prosím znovu");
+        console.error(data.error);
+    }
+}
 
 export default function Voting(){
     const searchParams = useSearchParams();
     const token = searchParams.get('token');
 
+    if (token !== null){
+        login(null, null, token);
+    }
 
     return(
         <div className="main-container">
--- frontend/app/voting/page.tsx
+++ frontend/app/voting/page.tsx
@@ -1,41 +1,44 @@
+'use server'
+
 import '../style/voting.css'
 import { cookies } from 'next/headers'
-import { redirect } from 'next/navigation';
+import { redirect } from 'next/navigation'
 import Film from './film';
 
 export default async function Voting() {
     const cookieStore = cookies()
     const token = cookieStore.get('token')
+    let data: APIResponse
 
-    let response = await fetch('https://apalucha.kaktusgame.eu/api/voting', { headers: { 'Cookie': `token=${token?.value}` } })
     try{
-        let data: APIResponse = await response.json() as APIResponse
-        if (data.error === "Failed to authenticate" || data.error === "Token not found") {
-            return redirect('/login')
-        }
-        else if (data.error === "Voting has not started") {
-            return (
-                <div className="main-container">
-                    <h1>Voting has not started yet</h1>
-                </div>
-            );
-        }
-
-        return (
-            <div className="main-container">
-                <h1>Koho dnes zvolíš?</h1>
-                <Film data={data} />
-            </div>
-        );
+        let response = await fetch('https://apalucha.kaktusgame.eu/api/voting', { headers: { 'Cookie': `token=${token?.value}` } })
+        data = await response.json() as APIResponse
     }
-    catch (e) {
-        console.error(e)
+    catch (error){
+        console.error(error)
         return(
             <div className="main-container">
                 <h1>Něco se pokazilo, zkuste to prosím znovu</h1>
             </div>
         )
     }
+    if (data.error === "Failed to authenticate" || data.error === "Token not found") {
+        redirect('/login')
+    }
+    else if (data.error === "Voting has not started") {
+        return (
+            <div className="main-container">
+                <h1>Voting has not started yet</h1>
+            </div>
+        );
+    }
+
+    return (
+        <div className="main-container">
+            <h1>Koho dnes zvolíš?</h1>
+            <Film data={data} />
+        </div>
+    );
 
     function buttonClicked() {
         const buttons = document.querySelectorAll('button')
@@ -43,5 +46,4 @@ export default async function Voting() {
             button.disabled = false
         })
     }
-
 }
