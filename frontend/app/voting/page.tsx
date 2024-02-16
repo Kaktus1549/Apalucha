8c67b5342e5f84e1df70b342f0c4d2029fd5f98a
Kaktus1549 <hummelotup@seznam.cz>
2024-02-16 16:55:31 +0100

    fix?

--- frontend/app/voting/page.tsx
+++ frontend/app/voting/page.tsx
@@ -11,7 +11,8 @@ export default async function Voting() {
     let data: APIResponse
 
     try{
-        let response = await fetch('https://apalucha.kaktusgame.eu/api/voting', { headers: { 'Cookie': `token=${token?.value}` } })
+        let api_url = process.env.URL + '/api/voting'
+        let response = await fetch(api_url, { headers: { 'Cookie': `token=${token?.value}` } })
         data = await response.json() as APIResponse
     }
     catch (error){
