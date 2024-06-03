5c7ba84133b6ad4640fb3ee3d34563af2f98c66f
BuildTools <unconfigured@null.spigotmc.org>
2024-06-03 18:24:27 +0200

    Logging messages change

--- Backend/route.py
+++ Backend/route.py
@@ -423,13 +423,13 @@ def managment():
             # All stored in environment variables
             logger = getenv("WEBHOOK_LOGGER")
             if logger != str(webhooks_data["webhook_logging"]):
-                log("INFO", f"Admin \"{user}\" from IP address {ip} changed webhook logging from {logger} to {webhooks_data['webhook_logging']}")
+                log("INFO", f"Admin \"{user}\" from IP address {ip} changed webhook logging from \"{logger}\" to \"{webhooks_data['webhook_logging']}\"")
                 environ["WEBHOOK_LOGGER"] = str(webhooks_data["webhook_logging"])
                 changed_anything = True
         if webhooks_data["url"] != None:
             url = getenv("DISCORD_WEBHOOK_URL")
             if url != str(webhooks_data["url"]):
-                log("INFO", f"Admin \"{user}\" from IP address {ip} changed webhook url from {url} to {webhooks_data['url']}")
+                log("INFO", f"Admin \"{user}\" from IP address {ip} changed webhook url from \"{url}\" to \"{webhooks_data['url']}\"")
                 environ["DISCORD_WEBHOOK_URL"] = str(webhooks_data["url"])
                 changed_anything = True
 
