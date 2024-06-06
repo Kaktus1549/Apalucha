c35599d07f7e2607e92830f23ae69b118e9c2fc4
BuildTools <unconfigured@null.spigotmc.org>
2024-06-06 10:58:55 +0200

    Films fix + code optimalization

--- Frontend/app/scoreboard/Films.tsx
+++ Frontend/app/scoreboard/Films.tsx
@@ -6,58 +6,65 @@ function endDelay(order: number, filmLength: number){
   let time = max - order;
   return time + "s" as string;
 }
+function voteRunFilm(i: number, filmKeys: string[], films: Films, delay: number){
+  return(
+      <>
+        <div style={{animationDelay: (i/delay).toString() + "s"}} key={"fimls-" + i.toString()} className="film">
+          <p className="id">{filmKeys[i]}</p>
+          <p className="name">{films[parseInt(filmKeys[i])]}</p>
+        </div>
+      </>
+    );
+}
+function endedRunFilm(i: number, filmKeys: string[], films: Films, votes: number[]){
+  if (i === 0){
+    return(
+      <>
+        <div style={{animationDelay: endDelay(i + 1, filmKeys.length)}} key={"winning-aura"} className="winning-aura hidden" id={(i+1).toString()}>
+          <div className="film" key="winning-film">
+            <p className="id">{i + 1}.</p>
+            <p className="name">{films[parseInt(filmKeys[i])]}</p>
+            <p className="votes">{votes[i]}</p>
+          </div>
+        </div>
+      </>
+    );
+  }
+  else{
+    return(
+      <>
+        <div style={{animationDelay: endDelay(i + 1, filmKeys.length)}} className={ [1, 2, 3].includes((i + 1)) ? "film hidden" : "film"} key={i + 1} id={(i+1).toString()}>
+          <p className="id">{i + 1}.</p>
+          <p className="name">{films[parseInt(filmKeys[i])]}</p>
+          <p className="votes">{votes[i]}</p>
+        </div>
+      </>
+    );
+  
+  }
+}
 
 
 export default function DisplayFilms({ inputFilms, ended, votes }: { inputFilms: Films; ended: boolean; votes: number[];}) {
   const delay = 3.5;
-  const finalDelay = 1;
   let returnFilms = [];
   let length = Object.keys(inputFilms).length;
+  let keys = Object.keys(inputFilms);
   if (length === 0) {
     return <></>;
   }
   if (ended === false) {
     let third = Math.floor(Object.keys(inputFilms).length / 3);
     for (let i = 0; i < third; i++) {
-      returnFilms.push(
-        <div style={{animationDelay: (i/delay).toString() + "s"}} key={"fimls-" + i.toString()} className="film">
-          <p className="id">{i + 1}</p>
-          <p className="name">{inputFilms[i + 1]}</p>
-        </div>
-      );
-      returnFilms.push(
-        <div style={{animationDelay: ((i + third)/delay).toString() + "s"}} key={(i + third).toString()} className="film">
-          <p className="id">{i + third + 1}</p>
-          <p className="name">{inputFilms[i + third + 1]}</p>
-        </div>
-      );
-      returnFilms.push(
-        <div style={{animationDelay: ((i + third *2)/delay).toString() + "s"}} key={(i + 2 * third).toString()} className="film">
-          <p className="id">{i + 2 * third + 1}</p>
-          <p className="name">{inputFilms[i + 2 * third + 1]}</p>
-        </div>
-      );
+      returnFilms.push(voteRunFilm(i, keys, inputFilms, delay));
+      returnFilms.push(voteRunFilm(i + third, keys, inputFilms, delay));
+      returnFilms.push(voteRunFilm(i + 2 * third, keys, inputFilms, delay));
     }
     if (length % 3 === 1) {
-      returnFilms.push(
-        <div style={{animationDelay: ((length-1)/delay).toString() + "s"}} key={(length - 1).toString()} className="film">
-          <p className="id">{length}</p>
-          <p className="name">{inputFilms[length]}</p>
-        </div>
-      );
+      returnFilms.push(voteRunFilm(length - 1, keys, inputFilms, delay));
     } else if (length % 3 === 2) {
-      returnFilms.push(
-        <div style={{animationDelay: ((length-2)/delay).toString() + "s"}} key={(length - 2).toString()} className="film">
-          <p className="id">{length - 1}</p>
-          <p className="name">{inputFilms[length - 1]}</p>
-        </div>
-      );
-      returnFilms.push(
-        <div style={{animationDelay: ((length-1)/delay).toString() + "s"}} key={(length - 1).toString()} className="film">
-          <p className="id">{length}</p>
-          <p className="name">{inputFilms[length]}</p>
-        </div>
-      );
+      returnFilms.push(voteRunFilm(length - 2, keys, inputFilms, delay));
+      returnFilms.push(voteRunFilm(length - 1, keys, inputFilms, delay));
     }
     return returnFilms;
   }
@@ -67,68 +74,24 @@ export default function DisplayFilms({ inputFilms, ended, votes }: { inputFilms:
     let nonWinningFilms = [];
     for (let i = 0; i < half; i++) {
       if (i === 0) {
-        winningFilm.push(
-          <div style={{animationDelay: endDelay(i + 1, length)}} key={"winning-aura"} className="winning-aura hidden" id={(i+1).toString()}>
-            <div className="film" key="winning-film">
-              <p className="id">{i + 1}.</p>
-              <p className="name">{inputFilms[i + 1]}</p>
-              <p className="votes">{votes[i]}</p>
-            </div>
-          </div>
-        );
+        winningFilm.push(endedRunFilm(i, keys, inputFilms, votes));
       }
       else {
-        nonWinningFilms.push(
-          <div style={{animationDelay: endDelay(i + 1, length)}} className={ [1, 2, 3].includes((i + 1)) ? "film hidden" : "film"} key={i + 1} id={(i+1).toString()}>
-            <p className="id">{i + 1}.</p>
-            <p className="name">{inputFilms[i + 1]}</p>
-            <p className="votes">{votes[i]}</p>
-          </div>
-        );
+        nonWinningFilms.push(endedRunFilm(i, keys, inputFilms, votes));
         if (length % 2 === 0) {
-          nonWinningFilms.push(
-            <div style={{animationDelay: endDelay(i + half, length)}} className={ [1, 2, 3].includes((i + half)) ? "film hidden" : "film"} key={i + half} id={(i+half).toString()}>
-              <p className="id">{i + half}.</p>
-              <p className="name">{inputFilms[i + half]}</p>
-              <p className="votes">{votes[i + half - 1]}</p>
-            </div>
-          );
+          nonWinningFilms.push(endedRunFilm(i + half - 1, keys, inputFilms, votes));
         }
         else {
-          nonWinningFilms.push(
-            <div style={{animationDelay: endDelay(i + half + 1, length)}} className={ [1, 2, 3].includes((i + half + 1))? "film hidden" : "film"} key={i + half + 1} id={(i+half+1).toString()}>
-              <p className="id">{i + half + 1}.</p>
-              <p className="name">{inputFilms[i + half + 1]}</p>
-              <p className="votes">{votes[i + half]}</p>
-            </div>
-          );
+          nonWinningFilms.push(endedRunFilm(i + half, keys, inputFilms, votes));
         }
       }
     }
     if (length % 2 === 1) {
-      nonWinningFilms.push(
-        <div style={{animationDelay: endDelay(half + 1, length)}} className={ [1, 2, 3].includes((half + 1)) ? "film hidden" : "film"} key={half + 1} id={(half+1).toString()}>
-          <p className="id">{half + 1}.</p>
-          <p className="name">{inputFilms[half + 1]}</p>
-          <p className="votes">{votes[half]}</p>
-        </div>
-      );
-      nonWinningFilms.push(
-        <div style={{animationDelay: endDelay(length, length)}} className={ [1, 2, 3].includes(length)? "film hidden" : "film"} key={length} id={length.toString()}>
-          <p className="id">{length}.</p>
-          <p className="name">{inputFilms[length]}</p>
-          <p className="votes">{votes[length - 1]}</p>
-        </div>
-      );
+      nonWinningFilms.push(endedRunFilm(half, keys, inputFilms, votes));
+      nonWinningFilms.push(endedRunFilm(length-1, keys, inputFilms, votes));
     }
     else if (length % 2 === 0) {
-      nonWinningFilms.push(
-        <div style={{animationDelay: endDelay(length, length)}} className={ [1, 2, 3].includes(length) ? "film hidden" : "film"} key={length} id={length.toString()}>
-          <p className="id">{length}.</p>
-          <p className="name">{inputFilms[length]}</p>
-          <p className="votes">{votes[length - 1]}</p>
-        </div>
-      );
+      nonWinningFilms.push(endedRunFilm(length-1, keys, inputFilms, votes));
     }
     returnFilms.push(
       <>
