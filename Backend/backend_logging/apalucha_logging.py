5c0c18caa680b1daf068981911ee5175528a742e
Kaktus1549 <hummelotup@seznam.cz>
2024-07-19 18:06:53 +0200

    STR error fix

--- Backend/backend_logging/apalucha_logging.py
+++ Backend/backend_logging/apalucha_logging.py
@@ -141,7 +141,7 @@ def ballot_box_log(level, message):
         response = webhook.execute()
         if response.status_code == 429:
             retry_after = response.json().get('retry_after', 1)  # default to 1 second if not specified
-            time.sleep(retry_after)
+            time.sleep(int(retry_after))
             response = webhook.execute()
 
         if response.status_code != 200:
